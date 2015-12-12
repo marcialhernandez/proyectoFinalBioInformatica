4b1512031c9bfa6b9beca75f3ea69e677d1c521c
Marcial Hernandez <marcial.hernandez@usach.cl>
2015-12-12 20:57:13 -0300

    Proceso de diferenciación multiclase usando Kruskall-Wallis V1
    -Faltan detalles, todavía falta para que funcione correctamente

--- main.R
+++ main.R
@@ -7,9 +7,11 @@ library(Biobase)
 library(GEOquery)
 library(genefilter)
 #install.packages("ISLR")
+#install.packages("pgirmess")
 library(ISLR)
 library(made4)
 library(ade4)
+#library(pgirmess)
 
 cargaDatos<- function(archivo) { load(archivo); as.list(environment()) }
 
@@ -29,9 +31,48 @@ EtiquetasGenes<-NCI60$Annot
 
 #Datos completos obtenidos desde
 #http://www.bioinf.ucd.ie/people/aedin/R/full_datasets/
-dataComplete<-cargaDatos("NCI60.rda")
+dataComplete<-assayData(cargaDatos("NCI60.rda"))
 
 #Tabla que indica la pertenencia de la clase por cada probe
-dataComplete.clases<-dataComplete$NCI60$classes
+#dataComplete.clases<-dataComplete$NCI60$classes
 #Tabla de expresion de genes, etiquetada segun la identificacion de Affymetrix
 dataComplete.affy<-dataComplete$NCI60$Affy
+#NSCLC Corresponde al cancer de pulmon
+cantidadProbes<-dim(dataComplete.affy)[2]
+cantidadGenes<-dim(dataComplete.affy)[1]
+nombresClases<-c()
+for (nombreProbe in colnames(dataComplete.affy)){
+  nombresClases<-c(nombresClases,strsplit(nombreProbe,"_")[[1]][1])
+}
+#Se ha renonmbrado las clases, quedando todos los grupos con nombres comunes
+colnames(dataComplete.affy) <- nombresClases
+#Se obtiene una lista sin repeticiones del nombre de las clases
+nombresClases<-unique(nombresClases)
+matrizUbicacionesClases=c()
+for (nombreClase in nombresClases){
+  vectorTemporal <- which(colnames(dataComplete.affy)==nombreClase)
+  #print(vectorTemporal)
+  matrizUbicacionesClases<-c(matrizUbicacionesClases,vectorTemporal)
+}
+#Proceso de diferenciacion multiclase usando Kruskall-Wallis
+pvalue<-0
+
+for(a in 1:cantidadGenes) {
+  
+  mat_exp_t<-c()
+  for (posClases in  matrizUbicacionesClases){
+    if (a==1){
+    #print (dataComplete.affy[posClases])
+    }
+    c(mat_exp_t, dataComplete.affy[a,posClases])
+    
+  }
+  tabla<-data.frame(cbind(mat_exp_t,nombresClases))
+  pvalue[a]<-kruskal.test(tabla[,1]~nombresClases,data=tabla)$p.value
+}
+
+dataComplete.affyOrdenada<-dataComplete.affy[order(pvalue),]
+#dataComplete.affyOrdenada<-dataComplete.affy[order(pvalue)]
+#pvalue<-pvalue[order(pvalue)]
+
+#print(length(which(pvalue<=0.05)))
